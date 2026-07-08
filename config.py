8e093591b15fbcf7790fc169fe8f14ec2bcdb09c
Hpirmoradi <86420082+Hpirmoradi@users.noreply.github.com>
2026-07-09 00:33:14 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,21 @@
-DPI = 300
+from pathlib import Path
 
+# مسیر PDFها
+INPUT_FOLDER = Path(r"C:\Users\M\Downloads\New folder")
+
+# پوشه خروجی
+OUTPUT_FOLDER = Path("output")
+OUTPUT_FOLDER.mkdir(exist_ok=True)
+
+# کیفیت تصویر
+DPI = 300
 ZOOM = DPI / 72
 
+# تنظیمات برش
 LEFT = 350
 TOP = 420
 RIGHT = 250
 BOTTOM = 2200
+
+# نمایش زمان اجرا
+SHOW_TIME = True
